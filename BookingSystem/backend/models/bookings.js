421dd00c35c3cc8a7070dd0d03a77d1b78d2a2f0
Dhar-21-06 <dharshinis.aiml2023@citchennai.net>
2025-06-13 14:07:23 +0530

    bookings.jsx

--- BookingSystem/backend/models/bookings.js
+++ BookingSystem/backend/models/bookings.js
@@ -31,4 +31,4 @@ const bookingSchema = new mongoose.Schema({
   timestamps: true
 });
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model('Booking', bookingSchema); 
